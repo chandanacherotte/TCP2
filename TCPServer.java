6f2737ebdeb3e0917bd079424cc85f2d073dfe80
Anjali <anjalitbaiju@gmail.com>
2018-03-24 11:19:21 +0530

    Server created

---
+++ TCPServer.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.net.*;
+
+class TCPServer {
+ public static void main(String argv[]) throws Exception {
+  String clientSentence;
+  String capitalizedSentence;
+  ServerSocket welcomeSocket = new ServerSocket(6789);
+  }
+  }
