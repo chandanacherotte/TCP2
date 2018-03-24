eb2b3e37a547e262af5fbc3efe9a5a87645ee87e
Anjali <anjalitbaiju@gmail.com>
2018-03-24 12:01:03 +0530

    Server modified

--- TCPServer.java
+++ TCPServer.java
@@ -4,11 +4,11 @@ import java.net.*;
 class TCPServer {
  public static void main(String argv[]) throws Exception {
   String clientSentence;
-  String capitalizedSentence;
   ServerSocket welcomeSocket = new ServerSocket(6789);
+  Socket welcomeSocket=ss.accept();
   System.out.println("Welcome Client");
-  while (true) {
-   Socket connectionSocket = welcomeSocket.accept();
-   }
+  DataInputStream din=new DataInputStream(s.getInputStream());  
+    DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
+    BufferedReader br=new BufferedReader(new InputStreamReader(System.in)); 
   }
   }
