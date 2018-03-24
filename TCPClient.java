91c540b9c92b7f63641fff940175332fe00bd690
chandana <chandanacherotte@gmail.com>
2018-03-24 11:33:51 +0530

    edited, commit

--- TCPClient.java
+++ TCPClient.java
@@ -5,3 +5,11 @@ class TCPClient {
  public static void main(String argv[]) throws Exception {
   String sentence;
   String modifiedSentence;
+  BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+  Socket clientSocket = new Socket("localhost", 6789);
+System.out.println("Enter a message: ");
+  DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+  BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+  sentence = inFromUser.readLine();
+
+}}
